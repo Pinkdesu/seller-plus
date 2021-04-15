953df5a993dd47c48405127d6067da5da34685da
Pinkdesu <pinkdesu@yandex.ru>
2021-04-15 21:18:34 +0500

    refactor: delete all pages

--- src/features/AppBootstrapp/components/Sign.jsx
+++ src/features/AppBootstrapp/components/Sign.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from '~/features/Login';
-import Register from '~/features/Register';
-import Shop from '~/features/Shop';
-import DefaultView from '../../DefaultView';
+import DefaultView from '~/features/DefaultView';
 
 const Sign = () => {
   return (
     <DefaultView>
-      <Switch>
-        <Route path="/" component={Login} exact />
-        <Route path="/registration" component={Register} exact />
-        <Route path="/shop" component={Shop} exact />
-        <Route path="/rating" component={Login} exact />
-        <Route path="/task" component={Login} exact />
-        <Route path="/profile" component={Login} exact />
-      </Switch>
+      <Switch></Switch>
     </DefaultView>
   );
 };
--- src/features/DefaultView/index.jsx
+++ src/features/DefaultView/index.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import Footer from '~/features/Footer';
-import './style.scss';
 
 const DefaultView = (props) => {
-  const { children, footer = true } = props;
+  const { children } = props;
 
-  return (
-    <div className="wrapper">
-      {children}
-      {footer && <Footer />}
-    </div>
-  );
+  return <div className="wrapper">{children}</div>;
 };
 
 export default DefaultView;
